26bf73126a56045edb143f642c9f54db561c99ed
alotofhee <ekgmljeong@gmail.com>
2025-08-19 11:16:32 +0900

    feat: PGS_42890_후보키

--- DaheeJeong/Week1/PGS_후보키.py
+++ DaheeJeong/Week1/PGS_후보키.py
@@ -2,7 +2,7 @@ from itertools import combinations
 
 
 def solution(relation):
-    answer = []
+    answer = []  # 후보키 목록(조합)을 담음
     col, row = len(relation[0]), len(relation)
 
     # 인덱스 모든 조합
@@ -10,10 +10,17 @@ def solution(relation):
     for i in range(1, col + 1):
         combi.extend(combinations(range(col), i))
 
-    # 유일성 만족 여부
     for comb in combi:
-        lst = []
-        for item in relation:
-            print(item)
+        # 최소성 만족 여부: 기존 후보키가 현재 조합의 부분집합이면 스킵
+        if any(set(ck).issubset(comb) for ck in answer):
+            continue
 
-    return answer
+        # 유일성 만족 여부
+        keys = set()
+        for item in relation:
+            keys.add(tuple(item[i] for i in comb))
+
+        if len(keys) == row:
+            answer.append(comb)
+
+    return len(answer)
