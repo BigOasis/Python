3dc07cd7769ec37934f59c82c8c69960b32aaca0
abyss-s <lh3729@naver.com>
2025-08-24 21:34:39 +0900

    update: LTC_77_Combinations #3

--- YoungjuLee/Week1/LTC_77_Combinations.py
+++ YoungjuLee/Week1/LTC_77_Combinations.py
@@ -1,21 +1,15 @@
 """
-프로그래머스 87946번: 피로도
+리트코드 78번: Combinations
 """
-from itertools import permutations
+from itertools import combinations
 
-def solution(k, dungeons):
-    res = 0
-    for order in permutations(dungeons, len(dungeons)):
-        hp = k
-        cnt = 0
-        for n, c in order:
-            if hp >= n:
-                hp -= c
-                cnt += 1
-            else:
-                break
-        res = max(res, cnt)
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return [list(c) for c in combinations(range(1, n+1),k)]
 
-    return res
-
-# print(solution(80, [[80, 20], [50, 40], [30, 10]]))
+print(Solution().combine(4,2))
