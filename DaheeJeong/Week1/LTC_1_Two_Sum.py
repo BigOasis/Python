d2c98e32f1884876a39249ba9a7c21f6db9af141
alotofhee <ekgmljeong@gmail.com>
2025-08-26 09:43:40 +0900

    feat: LTC_1_Two_Sum

---
+++ DaheeJeong/Week1/LTC_1_Two_Sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        sorted_nums = sorted(nums)
+        answer = []
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            if sorted_nums[left] + sorted_nums[right] > target:
+                right -= 1
+            elif sorted_nums[left] + sorted_nums[right] < target:
+                left += 1
+            else:
+                answer.append(nums.index(sorted_nums[left]))
+                if sorted_nums[left] == sorted_nums[right]:
+                    answer.append(nums.index(sorted_nums[right], (nums.index(sorted_nums[left])) + 1, len(nums)))
+                else:
+                    answer.append(nums.index(sorted_nums[right]))
+                break
+
+        return answer
