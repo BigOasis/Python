47091ce018307264725ea75d6af5fd8c1b530c6c
alotofhee <ekgmljeong@gmail.com>
2025-09-02 11:03:06 +0900

    feat: PGS_17680_[1차]_캐시

---
+++ DaheeJeong/Week1/PGS_17680_[1차]_캐시.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    if cacheSize == 0:
+        return len(cities) * 5
+
+    caches = deque(maxlen=cacheSize)
+    cities = map(lambda x: x.lower(), cities)
+
+    for city in cities:
+        if city not in caches:
+            answer += 5
+            if len(caches) >= cacheSize:
+                caches.popleft()
+        else:
+            answer += 1
+            caches.remove(city)
+
+        caches.append(city)
+
+    return answer
