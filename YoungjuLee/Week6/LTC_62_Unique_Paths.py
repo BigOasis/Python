0012a6afb8cc4f024a647d21ed31742c9fad1e2e
abyss-s <lh3729@naver.com>
2025-09-25 02:51:56 +0900

    feat: LTC_62_Unique_Paths #60

---
+++ YoungjuLee/Week6/LTC_62_Unique_Paths.py
@@ -0,0 +1,15 @@
+"""
+LeetCode 62번: Unique Paths
+30분
+"""
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n for _ in range(m)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
