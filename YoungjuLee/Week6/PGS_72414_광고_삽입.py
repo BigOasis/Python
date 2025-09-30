584c6dcd6b08d1f42821b48baea1a9cf6a8c2e3c
abyss-s <lh3729@naver.com>
2025-09-30 10:59:24 +0900

    refactor: PGS_72414_광고_삽입 #60

--- YoungjuLee/Week6/PGS_72414_광고_삽입.py
+++ YoungjuLee/Week6/PGS_72414_광고_삽입.py
@@ -5,28 +5,30 @@
 
 
 def solution(play_time, adv_time, logs):
-    # 재생 시간
+    # 재생 시간 (초 단위 변환)
     h, m, s = map(int, play_time.split(':'))
     play_arr = h * 3600 + m * 60 + s
 
-    # 광고 시간
+    # 광고 시간 (초 단위 변환)
     h2, m2, s2 = map(int, adv_time.split(':'))
     adv_arr = h2 * 3600 + m2 * 60 + s2
 
     arr = [0] * (play_arr + 2)
 
-    # 시청자 변화 기록
+    # 차분 배열로 변화 기록
     for log in logs:
-        h1, s1, s1 = map(int, log[:8].split(':'))
-        h2, s2, s2 = map(int, log[9:].split(':'))
-        s = h1 * 3600 + s1 * 60 + s1
-        e = h2 * 3600 + s2 * 60 + s2
-        # difference array
+        sh, sm, ss = map(int, log[:8].split(':'))
+        eh, em, es = map(int, log[9:].split(':'))
+        s = sh * 3600 + sm * 60 + ss
+        e = eh * 3600 + em * 60 + es
         arr[s] += 1
         arr[e] -= 1
 
+    # 각 초 시청자 수 누적합
     for i in range(1, play_arr + 1):
         arr[i] += arr[i - 1]
+
+    # 총 시청 시간 누적합
     res = [0] * (play_arr + 2)
     for i in range(1, play_arr + 1):
         res[i] = res[i - 1] + arr[i - 1]
