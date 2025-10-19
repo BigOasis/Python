f5ab9f48f23ba6b5aeaecefa344e8bdbfa2a934d
alotofhee <ekgmljeong@gmail.com>
2025-10-20 01:16:25 +0900

    feat:PGS_150368_이모티콘_할인행사 #77

---
+++ DaheeJeong/Week8/PGS_150368_이모티콘_할인행사.py
@@ -0,0 +1,20 @@
+from itertools import product
+
+def solution(users, emoticons):
+    answer = []
+    discount_list = [10, 20, 30, 40]
+
+    for case in product(discount_list, repeat=len(emoticons)):
+        result = [0, 0]
+        for user in users:
+            tmp = 0
+            for idx, sale in enumerate(case):
+                if sale >= user[0]:
+                    tmp += emoticons[idx] * (100 - sale) // 100
+            if tmp >= user[1]:
+                result[0] += 1
+            else:
+                result[1] += tmp
+        answer.append(result)
+    answer.sort(key=lambda x: (-x[0], -x[1]))
+    return answer[0]
