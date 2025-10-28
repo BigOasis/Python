993264cf18bc02f1477011b5eb78ef9d42f24404
alotofhee <ekgmljeong@gmail.com>
2025-10-28 11:12:26 +0900

    feat: PGS_389479_서버_증설_횟수 #95

---
+++ DaheeJeong/Week10/PGS_389479_서버_증설_횟수.py
@@ -0,0 +1,20 @@
+def solution(players, m, k):
+    answer = 0
+    server = [m] * (len(players))
+
+    for i, player in enumerate(players):
+        if player >= server[i]:
+            diff = player + 1 - server[i]
+            server_num = diff // m
+
+            if diff % m != 0 or diff == 0:
+                server_num += 1
+
+            length = i + k
+            if length > 24:
+                length = 24
+
+            for j in range(i, length):
+                server[j] += server_num * m
+            answer += server_num
+    return answer
