4ecb9fffa4371cdcf0c708a2c4fb825af66c13d3
alotofhee <ekgmljeong@gmail.com>
2025-10-28 17:33:57 +0900

    refactor: PGS_389479_서버_증설_횟수 #95

--- DaheeJeong/Week10/PGS_389479_서버_증설_횟수.py
+++ DaheeJeong/Week10/PGS_389479_서버_증설_횟수.py
@@ -1,20 +1,12 @@
 def solution(players, m, k):
-    answer = 0
-    server = [m] * (len(players))
+    server = []
+    for player in players:
+        total = sum((server + [1])[-k:]) * m
 
-    for i, player in enumerate(players):
-        if player >= server[i]:
-            diff = player + 1 - server[i]
-            server_num = diff // m
+        if total > player:
+            server.append(0)
+        else:
+            server_num = (player - total) // m + 1
+            server.append(server_num)
 
-            if diff % m != 0 or diff == 0:
-                server_num += 1
-
-            length = i + k
-            if length > 24:
-                length = 24
-
-            for j in range(i, length):
-                server[j] += server_num * m
-            answer += server_num
-    return answer
+    return sum(server)
